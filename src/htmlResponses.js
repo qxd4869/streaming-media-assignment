eb18a861424856a9da8f65e277925fae49f2a9f5
Quang Dao <qxd4869@g.rit.edu>
2017-09-11 21:04:53 -0400

    Final Update

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
@@ -8,4 +9,11 @@ const getIndex = (request, response) => {
   response.end();
 };
 
+const getPage2 = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page2);
+  response.end();
+};
+
 module.exports.getIndex = getIndex;
+module.exports.getPage2 = getPage2;
--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,59 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const getParty = (request, response) => {
-/*   loadFile(request, response, '../client/party.mp4', 'video/mp4'); */
-  const file = path.resolve(__dirname, '../client.party.mp4');
 
-  fs.stat(file, (err, stats) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        response.writeHead(404);
-      }
-      return response.end(err);
-    }
-
-    let range = request.headers.range;
-
-    if (!range) {
-      range = 'bytes=0-';
-    }
-
-    const positions = range.replace(/bytes/, '').split('-');
-
-    let start = parseInt(positions[0], 10);
-
-    const total = stats.size;
-    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-
-    if (start > end) {
-      start = end - 1;
-    }
-
-    const chunksize = (end - start) + 1;
-
-    response.writeHead(206, {
-      'Content-Range': `bytes  ${start}-${end}/${total}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
-    });
-
-    const stream = fs.createReadStream(file, { start, end });
-
-    stream.on('open', () => {
-      stream.pipe(response);
-    });
-
-    stream.on('error', (streamErr) => {
-      response.end(streamErr);
-    });
-
-    return stream;
-  });
-};
-
-/* const loadFile = (request, response, filePath, contentType) => {
+const loadFile = (request, response, filePath, contentType) => {
   const file = path.resolve(__dirname, filePath);
 
   fs.stat(file, (err, stats) => {
@@ -70,7 +19,7 @@ const getParty = (request, response) => {
       range = 'bytes=0-';
     }
 
-    const positions = range.replace(/bytes/, '').split('-');
+    const positions = range.replace(/bytes=/, '').split('-');
 
     let start = parseInt(positions[0], 10);
 
@@ -102,7 +51,22 @@ const getParty = (request, response) => {
 
     return stream;
   });
-}; */
+};
+
+
+const getParty = (request, response) => {
+  loadFile(request, response, '../client/party.mp4', 'video/mp4');	
+};
+
+const getBling = (request, response) => {
+  loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');	
+};
+
+const getBird = (requrest, response) => {
+	loadFile(request, response, '../client/bird.mp4', 'video/mp4')
+};
 
 
 module.exports.getParty = getParty;
+module.exports.getBling = getBling;
+module.exports.getBird = getBird;
